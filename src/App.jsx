349ae8cb3ad9fec5dfcf75357fda08cbc45d26ae
Dewan Asifur Rahman <dasifrahman@gmail.com>
2025-01-12 00:26:48 +0600

    profile page and settings page done

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 
 export default function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
+  console.log({theme});
 
   useEffect(() => {
     checkAuth();
@@ -29,7 +32,7 @@ export default function App() {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -56,5 +56,19 @@ export const useAuthStore = create((set) => ({
         } catch (err) {
             toast.error(err.response.data.message);
         }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        } catch (err) {
+            console.log("Error in update profile", err);
+            toast.error(err.response.data.message);
+        } finally {
+            set({ isUpdatingProfile: false });
+        }
     }
 }));
